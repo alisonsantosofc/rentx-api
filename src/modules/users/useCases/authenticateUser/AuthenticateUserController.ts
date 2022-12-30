f39d3ca8ca0157f6525896210f4c2bf489111b4a
alisonsantosofc <alisonsantoso22@gmail.com>
2022-12-30 12:13:36 -0300

    feat: create authenticate route

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { email, password } = request.body;
+
+      const authenticateUserUseCase = container.resolve(
+        AuthenticateUserUseCase
+      );
+
+      const userToken = await authenticateUserUseCase.execute({
+        email,
+        password,
+      });
+
+      return response.status(200).json(userToken);
+    } catch {
+      return response
+        .status(400)
+        .json({ error: "Email or password incorret." });
+    }
+  }
+}
+
+export { AuthenticateUserController };
