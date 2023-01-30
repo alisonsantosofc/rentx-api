ff308b62cb3f4f8e1b3a5125ae8f80ceba620607
alisonsantosofc <alisonsantoso22@gmail.com>
2023-01-30 20:21:57 -0300

    build: create seed for main user admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,19 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import { postgresDataSource } from "..";
+
+async function create() {
+  const id = uuidV4();
+  const hashedPassword = await hash("admin001", 8);
+
+  await postgresDataSource.query(
+    `INSERT INTO USERS(id, name, email, password, driver_license, admin, created_at)
+      values('${id}', 'admin', 'admin@autorenter.com', '${hashedPassword}', 'driver_license', true, 'now()')
+    `
+  );
+}
+
+create().then(() => {
+  console.log("Main user admin created!");
+});
