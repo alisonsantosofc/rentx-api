58674d52acfcdd4697be88689d87e43a1d40ace2
alisonsantosofc <alisonsantoso22@gmail.com>
2023-02-18 08:48:29 -0300

    feat: create specifications relationships table

---
+++ src/shared/infra/typeorm/migrations/1676719256760-CreateSpecificationsCars.ts
@@ -0,0 +1,71 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateSpecificationsCars1676719256760
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications_cars",
+        columns: [
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "specification_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "fk_car_specifications_cars",
+        referencedTableName: "cars",
+        referencedColumnNames: ["id"],
+        columnNames: ["car_id"],
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "fk_specification_specifications_cars",
+        referencedTableName: "specifications",
+        referencedColumnNames: ["id"],
+        columnNames: ["specification_id"],
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      "specifications_cars",
+      "fk_specification_specifications_cars"
+    );
+
+    await queryRunner.dropForeignKey(
+      "specifications_cars",
+      "fk_car_specifications_cars"
+    );
+
+    await queryRunner.dropTable("specifications_cars");
+  }
+}
